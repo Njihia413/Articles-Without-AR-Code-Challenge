53a665c94b20eac7e68ff2726ea5a4b32a49b4a7
Maureen Njihia <maureennjihia468@gmail.com>
2022-10-29 17:18:14 +0300

    Complete magazine class initialization

--- tools/console.rb
+++ tools/console.rb
@@ -6,10 +6,15 @@ end
 
 
 ###  WRITE YOUR TEST CODE HERE ###
+### Author class initialization test
 njihia = Author.new("Njihia")
 lyn = Author.new("Lyn")
 atara = Author.new("Atara")
 
+### Magazine class initialization test
+new_york_times = Magazine.new("New York Times", "News")
+forbes = Magazine.new("Forbes", "Entertainment")
+top_forty_under_forty = Magazine.new("Top 40 Under 40", "Inspirational")
 
 
 
