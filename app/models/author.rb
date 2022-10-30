e92235f558d4aafd1b95a2ef57d22dafa8ce0468
Maureen Njihia <maureennjihia468@gmail.com>
2022-10-30 19:04:52 +0300

    Complete author aggregate methods

--- app/models/author.rb
+++ app/models/author.rb
@@ -15,4 +15,16 @@ class Author
     @magazines.uniq
   end
 
+  def add_article(magazine, title)
+    if(magazine.is_a?(Magazine) && title.is_a?(String))
+      @articles  << Article.new(self, magazine, title)
+      @magazines << magazine
+    else
+      puts "Expected Magazine.class to be an Object, title.class to be a String"
+    end
+  end
+
+  def topic_areas
+    @magazines.map {|magazine| magazine.category}.uniq
+  end
 end
