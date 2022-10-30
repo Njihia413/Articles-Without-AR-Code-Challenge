6ef1d1eba04d1301d7fec0f7702a78d2d8a50ebd
Maureen Njihia <maureennjihia468@gmail.com>
2022-10-30 19:21:00 +0300

    Complete magazine aggregate methods

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -3,9 +3,13 @@ class Magazine
   @@all = []
 
   def initialize(name, category)
-    @name = name
-    @category = category
-    @@all << self
+    if (name.is_a?(String) && category.is_a?(String))
+      @name = name
+      @category = category
+      @@all << self
+    else
+      puts "Expected name.class and category.class to be a String"
+    end
   end
 
   def self.all
@@ -16,5 +20,18 @@ class Magazine
     articles_by_authors_in_this_magazine.map {|article| article.author}
   end
 
-  
+  def self.find_by_name(name)
+    @@all.find {|magazine| magazine.name == name}
+  end
+
+  def article_titles
+    articles_by_authors_in_this_magazine.map {|article| article.title}
+  end
+
+  def contributing_authors
+    all_magazine_authors = articles_by_authors_in_this_magazine.map {|article| article.author}
+    uniq_magazine_authors =  all_magazine_authors.uniq
+
+    uniq_magazine_authors.filter {|author|  all_magazine_authors.count(author) > 2}
+  end
 end
